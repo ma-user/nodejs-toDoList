550a8d5df1982ea38ca0de7d52354d11b5784a33
ma-user <73891478+ma-user@users.noreply.github.com>
2021-07-06 22:22:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const sql = require('mssql');
-const { response } = require('express');
 
 const app = express();
 app.set('view engine', 'ejs');
